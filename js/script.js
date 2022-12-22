2802f4d97dc2678c492034d09edb04af60f65758
PaCCiFFisT <pacciffist@gmail.com>
2022-12-23 00:19:22 +0200

    add anchor scroll

--- js/script.js
+++ js/script.js
@@ -1,28 +1,39 @@
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper(".swiper", {
   // Optional parameters
   preventInteractionOnTransition: true,
   autoHeight: true,
-  direction: 'vertical',
+  direction: "vertical",
   loop: true,
   speed: 10000,
-  slidesPerView: 'auto',
-  autoplay:{
-    delay:0,
-  }
-
+  slidesPerView: "auto",
+  autoplay: {
+    delay: 0,
+  },
 });
 
-const swiper1 = new Swiper('.swiper-1', {
+const swiper1 = new Swiper(".swiper-1", {
   // Optional parameters
   preventInteractionOnTransition: true,
   autoHeight: true,
-  direction: 'vertical',
+  direction: "vertical",
   loop: true,
   speed: 10000,
-  slidesPerView: 'auto',
-  autoplay:{
+  slidesPerView: "auto",
+  autoplay: {
     reverseDirection: true,
-    delay:0,
-  }
+    delay: 0,
+  },
+});
 
+// const body = document.getElementsByTagName("body");
+const goUpBtn = document.getElementById("go-top");
+
+document.addEventListener("scroll", (e) => {
+  scrollPosition = window.scrollY;
+  console.log(scrollPosition);
+  if (scrollPosition > 500) {
+    goUpBtn.classList.remove("hidden");
+  } else if (scrollPosition <= 500 && !goUpBtn.classList.contains("hidden")) {
+    goUpBtn.classList.add("hidden");
+  }
 });
